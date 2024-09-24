95695357a20d0cb3051db9f8c6ba90cb99adfc73
Ingo Hark <134280957+Flamingolo@users.noreply.github.com>
2024-09-24 18:02:13 +0300

    Update apiConfig.js
    fixed api mistake

--- frontend/src/config/apiConfig.js
+++ frontend/src/config/apiConfig.js
@@ -1,9 +1,9 @@
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://localhost:5173/api';
 
 export const API_CONFIG = {
-    towns: `${API_BASE_URL}/api/town/`,
-    dungeons: `${API_BASE_URL}/api/dungeon/`,
-    character: `${API_BASE_URL}/api/character/`,
-    items: `${API_BASE_URL}/api/item/`,
-    mobs: `${API_BASE_URL}/api/mob/`,
+    towns: `${API_BASE_URL}/town/`,
+    dungeons: `${API_BASE_URL}/dungeon/`,
+    character: `${API_BASE_URL}/character/`,
+    items: `${API_BASE_URL}/item/`,
+    mobs: `${API_BASE_URL}/mob/`,
 }
