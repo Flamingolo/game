ea09d1b4e114c03b8eb00874e24847a7225d081a
Ingo Hark <134280957+Flamingolo@users.noreply.github.com>
2024-09-24 17:38:35 +0300

    fixed error message

--- frontend/src/components/UI/Error.jsx
+++ frontend/src/components/UI/Error.jsx
@@ -4,7 +4,7 @@ const Error = ({ message }) => {
   return (
     <div className="flex items-center justify-center h-full bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
       <strong className="font-bold">Error: </strong>
-      <span className="block sm:inline">{message}</span>
+      <span className="block sm:inline">{message || 'An unknown error occurred.'}</span>
     </div>
   );
 };
